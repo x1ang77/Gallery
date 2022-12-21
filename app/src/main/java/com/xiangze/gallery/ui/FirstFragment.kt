f25078ed24cd10b5e57cf062431192c157b98d8e
Lo0425 <chingclo.lcc@gmail.com>
2022-12-21 09:48:46 +0800

    second commit

--- app/src/main/java/com/xiangze/gallery/ui/FirstFragment.kt
+++ app/src/main/java/com/xiangze/gallery/ui/FirstFragment.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Gallery
 import com.xiangze.gallery.R
+import com.xiangze.gallery.databinding.FragmentFirstBinding
 
 class FirstFragment : Fragment() {
+    private lateinit var binding: FragmentFirstBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
