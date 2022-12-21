1f12ea1876e67e0c31bcc73e5ad5be0140ff6736
Caaron <arn_0000@hotmail.com>
2022-12-21 09:51:03 +0800

    second commit

---
+++ app/src/main/java/com/xiangze/gallery/ui/FirstFragment.kt
@@ -0,0 +1,21 @@
+package com.xiangze.gallery.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.xiangze.gallery.R
+import com.xiangze.gallery.databinding.FragmentFirstBinding
+
+class FirstFragment : Fragment() {
+    private lateinit var binding : FragmentFirstBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentFirstBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/com/xiangze/gallery/ui/SecondFragment.kt
@@ -0,0 +1,21 @@
+package com.xiangze.gallery.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.xiangze.gallery.R
+import com.xiangze.gallery.databinding.FragmentSecondBinding
+
+class SecondFragment : Fragment() {
+    private lateinit var binding: FragmentSecondBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSecondBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+}
