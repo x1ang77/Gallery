def2a67095030875a3b447d311611c6d2272d2e8
Joel Seah <quanshenjoelmichael21s@forwardschool.edu.my>
2022-12-21 10:16:10 +0800

    Added image|file|directory checking

--- app/src/main/java/com/xiangze/gallery/fragments/HomeFragment.kt
+++ app/src/main/java/com/xiangze/gallery/fragments/HomeFragment.kt
@@ -1,15 +1,19 @@
 package com.xiangze.gallery.fragments
 
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.xiangze.gallery.R
 import com.xiangze.gallery.databinding.FragmentHomeBinding
+import java.io.File
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
+    private val images: MutableList<File> = mutableListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,4 +23,27 @@ class HomeFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val path = Environment.getExternalStorageDirectory().path
+        getImages(path)
+
+        Log.d("debugging", images.size.toString())
+    }
+
+    private fun getImages(path: String) {
+        val root = File(path)
+        val files = root.listFiles()?.toList() ?: listOf()
+        for (file in files) {
+            if (Regex(".jpg|.png|.jpeg").containsMatchIn(file.name)) {
+                images.add(file)
+            }
+        }
+        for (file in files) {
+            if (file.isDirectory) {
+                getImages(file.path)
+            }
+        }
+    }
 }
